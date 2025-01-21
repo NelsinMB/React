ee0e2d32ac8d99df71f282e821fb7d7aa1fd60ff
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-01-21 14:39:16 -0500

    Add beginning of TicTacToe implementation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
+import Tictactoe from './components/Tictactoe'; 
+
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Tictactoe />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
---
+++ src/components/Tictactoe.js
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import "../styles.css"
+
+export default function Board() {
+    return (
+        <>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+        </div>
+        </>
+    )
+
+}
+
+function Square({value}) {
+    const [value, setValue] = useState(null)
+
+    function handleClick() {
+        console.log('clicked!');
+    }
+
+    return (
+        <button 
+        className="square"
+        onClick={handleClick}
+        >
+
+            {value}
+
+
+        </button>
+    );
+}
+

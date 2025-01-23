dbb753eea27fb1890416ac973f6204f1ed9c5e62
Nelsin Martin-Burnett <73394875+NelsinMB@users.noreply.github.com>
2025-01-23 14:07:56 -0500

    Completes TicTacToe implementation

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -2,14 +2,34 @@ import {useState} from 'react'
 import "../styles.css"
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState(Array(9).fill(null));
-    const currentSquares = history[history.length - 1];
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+    const xIsNext = currentMove % 2 === 0;
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move; 
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
 
     return (
         <div className="game">
@@ -18,7 +38,7 @@ export default function Game() {
             </div>
 
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
 
@@ -29,7 +49,7 @@ export default function Game() {
 function Board({xIsNext, squares, onPlay}) {
     
     function handleClick(i) {
-        if (squares[i] || calculateWinner(squares)) { // Checks if the square has been clicked or if a winner exists
+        if ( calculateWinner(squares) || squares[i]) { // Checks if the square has been clicked or if a winner exists
             return;
         }
         const nextSquares = squares.slice();
